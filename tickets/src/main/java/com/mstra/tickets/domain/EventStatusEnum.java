1b6e8da62316ebf4e421a6c141a1711c5c392222
Amos Uzuagie <amosuzuagie@gmail.com>
2025-08-18 03:18:08 +0100

    feat(domain): add enums, entities, and generated equals/hashCode

---
+++ tickets/src/main/java/com/mstra/tickets/domain/EventStatusEnum.java
@@ -0,0 +1,5 @@
+package com.mstra.tickets.domain;
+
+public enum EventStatusEnum {
+    DRAFT, PUBLISHED, CANCELED, COMPLETED
+}
---
+++ tickets/src/main/java/com/mstra/tickets/domain/TicketValidationMethod.java
@@ -0,0 +1,5 @@
+package com.mstra.tickets.domain;
+
+public enum TicketValidationMethod {
+    QR_SCAN, MANUAL
+}
