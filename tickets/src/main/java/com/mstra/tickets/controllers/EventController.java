a1a080dfb2980d03cde121f2c772c9230133330e
Amos Uzuagie <amosuzuagie@gmail.com>
2025-08-25 01:01:40 +0100

    feat(event): add controller and global exception handling
    - Created EventController with create event endpoint
    - Added ErrorDto for standardized error responses
    - Implemented GlobalExceptionHandler to format all errors consistently

---
+++ tickets/src/main/java/com/mstra/tickets/controllers/EventController.java
@@ -0,0 +1,42 @@
+package com.mstra.tickets.controllers;
+
+import com.mstra.tickets.domain.CreateEventRequest;
+import com.mstra.tickets.domain.dtos.CreateEventRequestDto;
+import com.mstra.tickets.domain.dtos.CreateEventResponseDto;
+import com.mstra.tickets.domain.entities.Event;
+import com.mstra.tickets.mappers.EventMapper;
+import com.mstra.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(params = "/api/v1/events")
+public class EventController {
+
+    private final EventMapper eventMapper;
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDto createEventRequestDto
+    ) {
+        CreateEventRequest request = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+        Event createdEvent = eventService.createEvent(userId, request);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+        return new ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
+
+    }
+}
---
+++ tickets/src/main/java/com/mstra/tickets/domain/dtos/ErrorDto.java
@@ -0,0 +1,12 @@
+package com.mstra.tickets.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDto {
+    private String error;
+}
